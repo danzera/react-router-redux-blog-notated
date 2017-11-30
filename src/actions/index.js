1db8e93c3c3a0978f92ad3d86a090c4d89bb11fb
Dan Zera <dczera@gmail.com>
2017-11-30 12:13:46 -0600

    pull posts from API within action creator, format the data in our reducer to be returned to the Redux store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
+import axios from 'axios';
+
 export const FETCH_POSTS = 'FETCH_POSTS';
 
+const API_KEY = 'danqzera';
+const baseUrl = 'https://reduxblog.herokuapp.com/api';
+
 export function fetchPosts() {
+	const url = `${baseUrl}/posts?key=${API_KEY}`;
+	const request = axios.get(url);
+
 	return {
 		type: FETCH_POSTS,
-		payload: 'MEOW'
+		payload: request
 	}
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -4,9 +4,26 @@ export default function posts(state = null, action) {
 	switch (action.type) {
 		case FETCH_POSTS:
 			console.log('FETCH_POSTS action', action);
-			return state;
+			const posts = mapObjectsArrayToObject('id', action.payload.data);
+			console.log('posts mapped to object', posts);
+			return posts;
 		default:
 			console.log('some action...');
 			return state;
 	}
+}
+
+/**
+ * 
+ * @param {string} keyName - the name of the object key whose VALUE we should use for our new object's keys
+ * @param {Array} array - array of objects to be added to our new object
+ */
+function mapObjectsArrayToObject(keyName, array) {
+	console.log('mapping objects array...keyName', keyName, 'original array', array);
+	let result = {};
+	for (let i = 0; i < array.length; i++) {
+		const elemId = array[i][keyName];
+		result[elemId] = array[i];
+	}
+	return result;
 }
