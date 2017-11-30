4bdfe4c072aea911bed93da3575ed5b74a02cff7
Dan Zera <dczera@gmail.com>
2017-11-30 09:59:04 -0600

    add base action creator for fetching posts, include redux-promise middleware for handling action with a Promise

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			postsFetched: false,
+			posts: props.fetchPosts()
+		};
+
+		console.log('props', props);
+		console.log('state', this.state);
+	}
+
 	render() {
 		return (
 			<div>PostsIndex</div>
@@ -8,4 +23,12 @@ class PostsIndex extends Component {
 	}
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+	return { posts: state.posts };
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsIndex);
