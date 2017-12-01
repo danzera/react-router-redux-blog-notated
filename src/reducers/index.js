0028164e8863c5beb3e4f428708a63ac7d665e7d
Dan Zera <dczera@gmail.com>
2017-12-01 14:26:35 -0600

    import redux-form and wire it into our reducers for handling forms within our app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
+// can alias imports, as done here
+// formReducer is the variable name we use to reference the imported functionality from redux-form
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  // MUST USE THE KEYWORD OF "form" AS OUR KEY NAME FOR formReducer WHEN WIRING IT INTO OUR REDUCERS
+  // all different forms that we hook up in our components will assume that the formReducer is being applied to the "form" piece of state
+  form: formReducer
 });
 
 export default rootReducer;
