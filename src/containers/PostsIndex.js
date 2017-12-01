e27961c76c80b3ee79289c61a882b75336cd2b36
Dan Zera <dczera@gmail.com>
2017-11-30 19:36:34 -0600

    fetch data using componentDidMount() lifecycle method and render the list of posts once fetched

--- src/containers/PostsIndex.js
+++ src/containers/PostsIndex.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions'
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
 	constructor(props) {
@@ -11,13 +12,30 @@ class PostsIndex extends Component {
 		};
 	}
 
+	// componentDidMount() is one of the React lifecycle methods, of which there are several
+	// lifecycle methods are functions on class-based React components that are AUTOMATICALLY CALLED by React
+	// componentDidMount() runs immediately after the component renders to the DOM
+	// componentWillMount() is another possible place to fetch data, HOWEVER the render() function would still be run before the async request finishes
+	// also, if you need to use server side rendering, componentWillMount() would be run twice, once on the server and once on the client
+	// whereas componentDidMount() only runs once, on the client
+	// additionally, with ES6 class-based components, the constructor plays a similar role to componentWillMount()
+	// bottom line:
+	// 1. componentDidMount() makes it clear that data won't be loaded until AFTER the initial render()
+	// --> this reminds you to setup initial state properly so you don't end up with undefined state, which results in errors
+	// 2. should you need to use server-side rendering, componentDidMount() will only run once, on the client
+	// --> as opposed to componentWillMount(), which would run twice, once on the server and then again on the client
 	componentDidMount() {
-		this.props.fetchPosts().then(res => console.log(res));
+		// THIS IS WHERE WE FETCH OUR DATA!!!
+		this.props.fetchPosts().then(action => {
+			console.log('posts fetched', action);
+			console.log('props is now', this.props);
+			this.setState({ posts: this.props.posts });
+		});
 	}
 
 	renderPosts() {
-		console.log('this.state.posts', this.state.posts)
-		return [<div>1</div>,<div>1</div>,<div>1</div>];
+		console.log('this.state.posts', this.state.posts);
+		return _.map(this.state.posts, (post, id, posts) => <div key={id}>{post.title}</div>);
 	}
 
 	render() {
@@ -38,5 +56,5 @@ function mapStateToProps(state) {
 //   return bindActionCreators({ fetchPosts }, dispatch);
 // }
 // and passing this to the connect function
-// there will still be times when we want to define mapDispatchToProps() separately in order to do some type of computation
+// there will still be times when we want to define mapDispatchToProps() separately in order to do some type of computation regarding the action creator
 export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
