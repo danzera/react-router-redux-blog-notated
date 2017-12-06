04f2985f4a39d0438a5f88e4a71b81bc8a1dee3e
Dan Zera <dczera@gmail.com>
2017-12-06 00:18:15 -0600

    use ownProps within mapStateToProps function to provide only the state we need for the component, in this case only the specific post we're interested in as opposed to the entire list of posts

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -8,21 +8,38 @@ class ShowPost extends Component {
 		// action creator & reducer handle the assigning of data to our state
 		// and mapStateToProps taps into the Redux store to get access to the state changes
 		// so no need for a .then callback to handle the API response here
-		this.props.fetchPost(this.props.match.params.id);
+		// THIS IS HOW WE ACCESS OUR URL PARAMS
+		const { id } = this.props.match.params;
+		this.props.fetchPost(id);
 	}
 
 	render() {
 		console.log('this.props in ShowPost', this.props);
 		console.log('url params in ShowPost', this.props.match.params);
+		const post = this.props.post;
+		// conditionally show loading message until content is returned from the API
+		if (!post) {
+			return <div>Loading...</div>;
+		}
+
 		return (
-			<div>Show Post View</div>
+			<div>
+				<h2>{post.title}</h2>
+			</div>
 		);
 	}
 }
 
-function mapStateToProps(state) {
-	console.log('mapStateToProps state', state);
-	return { posts: state.posts };
+/**
+ * 
+ * @param {Object} posts - using destructuring to pull the posts object out of the state object (don't need the whole state object)
+ * @param {Object} ownProps - THERE'S ALSO A SECOND ARGUMENT in mapStateToProps called 'ownProps' by convention; corresponds to the props that are going/headed to the component
+ */
+function mapStateToProps({ posts }, ownProps) {
+	console.log('posts in ShowPost', posts);
+	console.log('ownProps in ShowPost', ownProps);
+	// can use own props to send back only the post that we want to show (provided by the id <Route /> param)
+	return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, { fetchPost })(ShowPost);
