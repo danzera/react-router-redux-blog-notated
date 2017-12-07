188970baa9d82706d170a2ccd38e7d6eb9a216ec
Dan Zera <dczera@gmail.com>
2017-12-07 16:20:31 -0600

    add button to ShowPost to return user to index view

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class ShowPost extends Component {
 	componentDidMount() {
@@ -25,6 +26,7 @@ class ShowPost extends Component {
 
 		return (
 			<div>
+				<Link to="/" className="btn btn-primary">All Posts</Link>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>{post.content}</p>
